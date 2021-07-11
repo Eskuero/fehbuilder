e20995bd8334bd1fe8368fe53e4e07e649ba41be
Óliver García Albertos <3skuero@gmail.com>
2021-07-11 23:00:17 +0100

    Patch image grab for heroes with ' or tildes in their titlename

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,9 +1,10 @@
 import bs4
 import requests
+import unidecode
 
 def obtaintrueurl(filename):
 	# Download the wiki page for the icon art since we can't reliably get the real link because it appears to be in folders with random numbers
-	artpage = bs4.BeautifulSoup(requests.get("https://feheroes.fandom.com/wiki/File:" + filename.replace(" ", "_").replace(":", "").replace("?", "")).text, 'html.parser')
+	artpage = bs4.BeautifulSoup(requests.get("https://feheroes.fandom.com/wiki/File:" + unidecode.unidecode(filename).replace(" ", "_").replace(":", "").replace("?", "").replace("'", "")).text, 'html.parser')
 	try:
 		# We get the full url by reading the doc item href value
 		return artpage.select_one('div.fullImageLink').select_one('a').get('href').split("/revision")[0]
