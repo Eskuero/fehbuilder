0434831991fdcbf52a3b985a0a85de0537639c27
Óliver García Albertos <3skuero@gmail.com>
2021-07-15 23:54:25 +0100

    Improve getting full url resilience against wiki changes
    Suddenly not all pages are served with a fullImageLink div item so it
    becomes harder to retrieve the full link, just look for it around the
    entire page instead

--- scripts/utils.py
+++ scripts/utils.py
@@ -3,11 +3,13 @@ import requests
 import unidecode
 
 def obtaintrueurl(filename):
+	# Cleaned up name for the file
+	name = unidecode.unidecode(filename).replace(" ", "_").replace(":", "").replace("?", "").replace("'", "")
 	# Download the wiki page for the icon art since we can't reliably get the real link because it appears to be in folders with random numbers
-	artpage = bs4.BeautifulSoup(requests.get("https://feheroes.fandom.com/wiki/File:" + unidecode.unidecode(filename).replace(" ", "_").replace(":", "").replace("?", "").replace("'", "")).text, 'html.parser')
+	artpage = bs4.BeautifulSoup(requests.get("https://feheroes.fandom.com/wiki/File:" + name).text, 'html.parser')
 	try:
 		# We get the full url by reading the doc item href value
-		return artpage.select_one('div.fullImageLink').select_one('a').get('href').split("/revision")[0]
+		return artpage.select_one("a[href*=" + name.replace(".", "\.").split("_")[-1] + "]").get('href').split("/revision")[0]
 	except:
 		# The referenced data art is probably incorrect and we couldn't get a full url, return here false and let the caller handle fallback values
 		return False
