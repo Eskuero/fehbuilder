0d3f727fede000ee616e9056ee6ce070aaf09d2d
Óliver García Albertos <3skuero@gmail.com>
2021-08-11 14:33:38 +0100

    Do not scroll back when switching slots

--- static/data.js
+++ static/data.js
@@ -214,7 +214,7 @@ function populate(select, data, clean, bypass) {
 	}
 }
 
-function reload() {
+function reload(scroll = true) {
 	// Obtain the list of support heroes
 	allies = "";
 	for (i = 0; i < selectallies.selectedOptions.length; i++) {
@@ -225,7 +225,9 @@ function reload() {
 	// Change the URL of the img to force it to reload
 	document.getElementById('fakecanvas').src = "/get_image.png?name=" + encodeURIComponent(selectheroes.value) + "&rarity=" + selectrarity.value + "&merges=" + selectmerges.value + "&flowers=" + selectflowers.value + "&boon=" + selectboons.value + "&bane=" + selectbanes.value + "&beast=" + selectbeast.value + "&weapon=" + encodeURIComponent(selectweapons.value) + "&refine=" + selectrefines.value + "&assist=" + encodeURIComponent(selectassists.value) + "&special=" + encodeURIComponent(selectspecials.value) + "&passiveA=" + encodeURIComponent(selectA.value) + "&passiveB=" + encodeURIComponent(selectB.value) + "&passiveC=" + encodeURIComponent(selectC.value) + "&passiveS=" + encodeURIComponent(selectS.value) + "&blessing=" + selectblessings.value + "&summoner=" + selectsummoner.value + "&attire=" + selectattire.value + "&appui=" + appui.checked + "&bonusunit=" + selectbonusunit.value + "&allies=" + encodeURIComponent(allies) + "&buffs=" + encodeURIComponent(buffs) + "&sp=" + selectsp.value + "&hm=" + selecthm.value + "&artstyle=" + selectartstyle.value + "&offsetY=" + selectoffsetY.value + "&offsetX=" + selectoffsetX.value + "&favorite=" + selectfavorite.value + "&accessory=" + selectaccessory.value + "&language=" + selectlanguage.value;
 	// Autoscroll all the way up so the user can inmediately see the hero preview on portrait screens
-	window.scrollTo(0, 0);
+	if (scroll) {
+		window.scrollTo(0, 0);
+	}
 }
 
 function beastcheck() {
@@ -479,7 +481,7 @@ function switchbuild(build) {
 		}
 	}
 	// Reload the image
-	reload();
+	reload(false);
 }
 function slotname() {
 	// Update unit name in the builder tab
