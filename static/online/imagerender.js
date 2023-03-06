fdf9e363b23377a95a287d3d008b01dbf6125272
Óliver García Albertos <3skuero@gmail.com>
2023-03-06 22:10:37 +0000

    builder: Cap HM at 9000

--- static/online/imagerender.js
+++ static/online/imagerender.js
@@ -761,7 +761,7 @@ async function myunit() {
 	// Print the amount of SP and HM
 	var numbertype = selectsp.value == "9999" ? 4 : 0;
 	printnumbers(preview, parseInt(selectsp.value), numbertype, 265, 1052, "end");
-	var numbertype = selecthm.value == "8000" ? 4 : 0;
+	var numbertype = selecthm.value == "9000" ? 4 : 0;
 	printnumbers(preview, parseInt(selecthm.value), numbertype, 265, 1100, "end");
 
 	// Print the ascendent floret icon if selected
