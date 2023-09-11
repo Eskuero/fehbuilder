de4f143bfc8daac95fd44948765f44a82de17287
Óliver García Albertos <3skuero@gmail.com>
2023-09-11 19:52:16 +0100

    scripts: Code cosmetic changes for sync scripts

--- scripts/hackin.sh
+++ scripts/hackin.sh
@@ -1,28 +1,45 @@
 #!/bin/sh
-date=$(date +"%Y-%m-%d-%H-%M-%S")
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Enviroment variables for python scripts
 export RENEWDATA_MODE="hackin_device"
+export PYTHONUNBUFFERED=1
 
-echo "Updating submodules..." >> fehupdate-$date.log 2>&1
-git submodule update --remote >> fehupdate-$date.log 2>&1;
+DATE=$(date +"%Y-%m-%d-%H-%M"-%S)
 
-echo -e "\n        - Other..." >> fehupdate-$date.log 2>&1
-PYTHONUNBUFFERED=1 python3 hackin-other.py >> fehupdate-$date.log 2>&1
-echo -e "\n        - Languages..." >> fehupdate-$date.log 2>&1
-PYTHONUNBUFFERED=1 python3 hackin-languages.py >> fehupdate-$date.log 2>&1
-echo -e "\n        - Skills..." >> fehupdate-$date.log 2>&1
-PYTHONUNBUFFERED=1 python3 skills.py >> fehupdate-$date.log 2>&1
-echo -e "\n        - Heroes..." >> fehupdate-$date.log 2>&1
-PYTHONUNBUFFERED=1 python3 hackin-units.py >> fehupdate-$date.log 2>&1
+echo "Updating submodules..." >> fehupdate-$DATE.log 2>&1
+git submodule update --remote >> fehupdate-$DATE.log 2>&1;
 
-echo -e "\n        - Copying outputs to destination..." >> fehupdate-$date.log 2>&1
+echo -e "\n        - Other..." >> fehupdate-$DATE.log 2>&1
+python3 hackin-other.py >> fehupdate-$DATE.log 2>&1
+echo -e "\n        - Languages..." >> fehupdate-$DATE.log 2>&1
+python3 hackin-languages.py >> fehupdate-$DATE.log 2>&1
+echo -e "\n        - Skills..." >> fehupdate-$DATE.log 2>&1
+python3 skills.py >> fehupdate-$DATE.log 2>&1
+echo -e "\n        - Heroes..." >> fehupdate-$DATE.log 2>&1
+python3 hackin-units.py >> fehupdate-$DATE.log 2>&1
+
+echo -e "\n        - Copying outputs to destination..." >> fehupdate-$DATE.log 2>&1
 mv *languages*.json ../data/languages/;
 mv *units.json *skills.json *other.json ../data/content/;
-echo -e "\n        - Compressing outputs..." >> fehupdate-$date.log 2>&1
+echo -e "\n        - Compressing outputs..." >> fehupdate-$DATE.log 2>&1
 gzip -fk ../data/languages/*json;
 gzip -fk ../data/content/*json;
 
-echo -e "\nExtracting missing assets from device..." >> fehupdate-$date.log 2>&1
-PYTHONUNBUFFERED=1 python3 hackin-assets.py >> fehupdate-$date.log 2>&1
+echo -e "\nExtracting missing assets from device..." >> fehupdate-$DATE.log 2>&1
+python3 hackin-assets.py >> fehupdate-$DATE.log 2>&1
 
-echo -e "\nBeautifying json outputs..." >> fehupdate-$date.log 2>&1
-PYTHONUNBUFFERED=1 python3 hackin-indenter.py >> fehupdate-$date.log 2>&1
+echo -e "\nBeautifying json outputs..." >> fehupdate-$DATE.log 2>&1
+python3 hackin-indenter.py >> fehupdate-$DATE.log 2>&1
--- scripts/renewdata.sh
+++ scripts/renewdata.sh
@@ -1,45 +1,62 @@
 #!/bin/sh
-date=$(date +"%Y-%m-%d-%H-%M"-%S)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Enviroment variables for python scripts
 export RENEWDATA_MODE="hertz_wiki"
+export PYTHONUNBUFFERED=1
+
+DATE=$(date +"%Y-%m-%d-%H-%M"-%S)
 
 if [[ "$2" != "--hackin" ]]; then
-	echo -e "---------------------------------------------------------" >> fehupdate-$date.log 2>&1
-	echo "Updating FeH datamined dumps..." >> fehupdate-$date.log 2>&1
+	echo -e "---------------------------------------------------------" >> fehupdate-$DATE.log 2>&1
+	echo "Updating FeH datamined dumps..." >> fehupdate-$DATE.log 2>&1
 	currenthead=$(git -C feh-assets-json/ rev-parse HEAD);
-	git submodule update --remote >> fehupdate-$date.log 2>&1;
+	git submodule update --remote >> fehupdate-$DATE.log 2>&1;
 
 	# Check if something was updated
 	newcurrenthead=$(git -C feh-assets-json/ rev-parse HEAD);
 fi
 
 if [[ "$currenthead" == "$newcurrenthead" ]] && [[ "$1" != "--force" ]]; then
-    echo -e "\n        - Datamined dumps were not updated." >> fehupdate-$date.log 2>&1
-	echo -e "\n---------------------------------------------------------" >> fehupdate-$date.log 2>&1
+    echo -e "\n        - Datamined dumps were not updated." >> fehupdate-$DATE.log 2>&1
+	echo -e "\n---------------------------------------------------------" >> fehupdate-$DATE.log 2>&1
 else
-    echo -e "\n        - Datamined dumps have been updated." >> fehupdate-$date.log 2>&1
-	echo -e "\n---------------------------------------------------------" >> fehupdate-$date.log 2>&1
-	echo "Parsing and preparing all necessary data:" >> fehupdate-$date.log 2>&1
-	echo -e "\n        - Other..." >> fehupdate-$date.log 2>&1
-	PYTHONUNBUFFERED=1 python3 renewdata-other.py >> fehupdate-$date.log 2>&1
-	echo -e "\n        - Languages..." >> fehupdate-$date.log 2>&1
-	PYTHONUNBUFFERED=1 python3 renewdata-languages.py >> fehupdate-$date.log 2>&1
-	echo -e "\n        - Skills..." >> fehupdate-$date.log 2>&1
-	PYTHONUNBUFFERED=1 python3 skills.py >> fehupdate-$date.log 2>&1
-	echo -e "\n        - Heroes..." >> fehupdate-$date.log 2>&1
-	PYTHONUNBUFFERED=1 python3 renewdata-units.py >> fehupdate-$date.log 2>&1
+    echo -e "\n        - Datamined dumps have been updated." >> fehupdate-$DATE.log 2>&1
+	echo -e "\n---------------------------------------------------------" >> fehupdate-$DATE.log 2>&1
+	echo "Parsing and preparing all necessary data:" >> fehupdate-$DATE.log 2>&1
+	echo -e "\n        - Other..." >> fehupdate-$DATE.log 2>&1
+	python3 renewdata-other.py >> fehupdate-$DATE.log 2>&1
+	echo -e "\n        - Languages..." >> fehupdate-$DATE.log 2>&1
+	python3 renewdata-languages.py >> fehupdate-$DATE.log 2>&1
+	echo -e "\n        - Skills..." >> fehupdate-$DATE.log 2>&1
+	python3 skills.py >> fehupdate-$DATE.log 2>&1
+	echo -e "\n        - Heroes..." >> fehupdate-$DATE.log 2>&1
+	python3 renewdata-units.py >> fehupdate-$DATE.log 2>&1
 	# FIXME: UGH
-	echo -e "\n        - Summoning pools..." >> fehupdate-$date.log 2>&1
-	PYTHONUNBUFFERED=1 python3 summoning.py >> fehupdate-$date.log 2>&1
-	echo -e "\n        - Copying outputs to destination..." >> fehupdate-$date.log 2>&1
+	echo -e "\n        - Summoning pools..." >> fehupdate-$DATE.log 2>&1
+	python3 summoning.py >> fehupdate-$DATE.log 2>&1
+	echo -e "\n        - Copying outputs to destination..." >> fehupdate-$DATE.log 2>&1
 	mv *skills.json *other.json *pools.json *units.json ../data/content/;
 	mv *languages*.json ../data/languages/;
-	echo -e "\n        - Compressing outputs..." >> fehupdate-$date.log 2>&1
+	echo -e "\n        - Compressing outputs..." >> fehupdate-$DATE.log 2>&1
 	gzip -fk ../data/content/*json;
 	gzip -fk ../data/languages/*json;
-	echo -e "\n---------------------------------------------------------" >> fehupdate-$date.log 2>&1
+	echo -e "\n---------------------------------------------------------" >> fehupdate-$DATE.log 2>&1
 fi
 
 if [[ "$2" != "--hackin" ]]; then
-	echo -e "Downloading missing assets from wiki..." >> fehupdate-$date.log 2>&1
-	PYTHONUNBUFFERED=1 python3 renewdata-assets.py >> fehupdate-$date.log 2>&1
+	echo -e "Downloading missing assets from wiki..." >> fehupdate-$DATE.log 2>&1
+	python3 renewdata-assets.py >> fehupdate-$DATE.log 2>&1
 fi
